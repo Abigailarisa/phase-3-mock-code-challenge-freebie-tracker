f25603745f874785b95e7e1c9eb8c5845125e2ea
Abigail Arisa <110755503+Abigailarisa@users.noreply.github.com>
2022-11-09 22:06:07 -0600

    completed

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,20 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def recieved_one?(item_name)
+        if self.freebies.where(item_name: item_name).count > 0
+            true
+        else
+            false
+        end
+
+    end
+
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id
+            freebie.dev_id = dev.id
+            freebie.save
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,19 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
+30.times do
+    Freebie.create(dev_id: Dev.all.sample.id, company_id: Company.all.sample.id, value: rand(1...100), item_name: Faker::Food.dish)
+
+end
+# Freebie.create(item_name: item_name, value: value, company_id: company_id, dev_id: dev_id)
+
+# t.string :item_name
+# t.integer :value
+# t.integer :dev_id
+# t.integer :company_id
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
+# * and a freebie belongs to a company.
 # ***************************************************************
 # Create freebies Here
 
